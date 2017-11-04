0638f197eb686d7232c64e8c12da2a01f64020e4
jarrm5 <rinellaj2448@gmail.com>
2017-11-04 15:50:57 -0500

    fix app.quit() issue

--- js/login.js
+++ js/login.js
@@ -5,16 +5,14 @@ $.getScript('../js/frame.js');
 
 //Recieving firebase data from main.js
 ipcRenderer.on('load_names', function(event,data){
-    //Take the firebase data and generate login panels dynamicaly
-    if($('#user_panels').children().length == 0) {
-        for (var u in data) {
-            $('#user_panels').append($('<div/>').addClass('panel')
-                .append($('<div/>').addClass('user_image')
-                    .append($('<img/>').attr('src','../images/' + data[u]['image'])))
-                .append($('<div/>').addClass('username').text(u)));
-        }
+    //Take the firebase data and generate login panels dynamicaly  
+    for (var u in data) {
+        $('#user_panels').append($('<div/>').addClass('panel')
+            .append($('<div/>').addClass('user_image')
+                .append($('<img/>').attr('src','../images/' + data[u]['image'])))
+            .append($('<div/>').addClass('username').text(u)));
     }
-
+    
     //Sending username to main.js for authentication
     $('.panel').click(function(){
         var usernameClicked = $(this).children('.username').text();
