5b3a16ba95bfa7abb0387c4d4617e541c24415c0
jarrm5 <rinellaj2448@gmail.com>
2017-11-21 16:47:34 -0600

    style index.js

--- js/index.js
+++ js/index.js
@@ -12,3 +12,8 @@ $('#btnreport').on('click',function(e){
     e.preventDefault();
     ipcRenderer.send('nav_report');
 });
+
+ipcRenderer.on('send_current_user', function(event,user){
+    var username = user['username'];
+    $('#user-header').text("Dashboard - " + username.substring(0,username.indexOf('@')));
+});
