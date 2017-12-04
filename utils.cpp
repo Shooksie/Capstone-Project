39cf8ef05c6ac8c279244f14ad17296c129482a4
Shooskie <ss9td@mail.umkc.edu>
2017-12-04 14:35:31 -0600

    fixes

--- utils.cpp
+++ utils.cpp
@@ -1,132 +1,42 @@
-#include <fstream>
 #include <iostream>
-#include <string>
-#include "utils.h"
-
 using namespace std;
 
-void getUsers(ifstream& ifs, User* &allUsers, int& nbrUsers, int& currentSize){
+struct student
+{
+    char name[50];
+    int roll;
+    float marks;
+} s[10];
 
-	if (ifs.fail())
-	{
-		throw UserException("Could not open the user file for reading.");
-	}
-
-	//While loop for reading in users and checkouts
-	while (!ifs.eof())
-	{
-		//Check the array size.  If the current index is the max capacity of the array, resize the user array.
-		if (nbrUsers == currentSize)
-		{
-			currentSize = expand(allUsers, nbrUsers);
-		}
-
-		User newUser;
-
-		//Get the user info from the text file
-		ifs >> newUser;
-		
-		//Make use of the assingment overload here.  Deep copies the user into the array.
-		allUsers[nbrUsers] = newUser;
-
-		//Since we added a user, increment the number of users count
-		nbrUsers++;
-
-		//The destructor will be called when we are out of scope bc the compiler thinks we don't need the user anymore.
-		//The deep copy works around this.
-	}
-}
-void checkIns(ifstream& cir, User* &allUsers, int& nbrUsers) throw(UserException){
-
-	if (cir.fail())
-	{
-		throw UserException("Could not open check ins for reading.");
-	}
-
-	//Read the isbns to be checked back in
-	while (!cir.eof())
-	{
-		//Capture each isbn number as we loop through the text file
-		bool found = false;
-		string isbn;
-		cir >> isbn;
-
-		//Try to check in each isbn for all users.
-		//If the user has the isbn in his check outlist, it is removed, otherwise, do nothing.
-		for (int i = 0; i < nbrUsers; i++)
-		{
-			if (allUsers[i].CheckIn(isbn))
-			{
-				found = true;
-				break;
-			} 
-		}
-
-		if (!found)
-		{
-			cerr << "No user could be found for ISBN " << isbn << endl;
-		}
-	}
-}
-void checkOuts(ifstream& cor, User* &allUsers, int& nbrUsers) throw(UserException){
-
-	if (cor.fail())
-	{
-		throw UserException("Could not open check outs for reading.");
-	}
-
-	while (!cor.eof())
-	{
-		bool found = false;
-		unsigned int id;
-		string isbn;
-
-		cor >> id;
-		cor >> isbn;
-
-		for (int i = 0; i < nbrUsers; i++)
-		{
-			if (allUsers[i].getid() == id)
-			{
-				found = true;
-				allUsers[i].CheckOut(isbn);
-				break;
-			}
-		}
-
-		if (!found)
-		{
-			cerr <<"ID " << id << " does not exist in our records." << endl;
-		}
-	}
-}
-void outputToFile(ofstream& fout, User* &allUsers, int& nbrUsers) throw(UserException){
-
-	if (fout.fail())
-	{
-		throw UserException("Could not open file for output.");
-	}
-
-	//Print the updated user array to the output file.
-	for (int i = 0; i < nbrUsers; i++)
-	{
-		fout << allUsers[i];
-	}
+int main()
+{
+    cout << "Enter information of students: " << endl;
+    
+    // storing information
+    for(int i = 0; i < 10; ++i)
+    {
+        s[i].roll = i+1;
+        cout << "For roll number" << s[i].roll << "," << endl;
+        
+        cout << "Enter name: ";
+        cin >> s[i].name;
+        
+        cout << "Enter marks: ";
+        cin >> s[i].marks;
+        
+        cout << endl;
+    }
+    
+    cout << "Displaying Information: " << endl;
+    
+    // Displaying information
+    for(int i = 0; i < 10; ++i)
+    {
+        cout << "\nRoll number: " << i+1 << endl;
+        cout << "Name: " << s[i].name << endl;
+        cout << "Marks: " << s[i].marks << endl;
+    }
+    
+    return 0;
 }
 
-int expand(User* &oldArray, int oldSize){
-
-	int newsize = oldSize * 2;
-
-	User *newArray = new User[newsize];
-
-	for (int i = 0; i < oldSize; i++)
-	{
-		newArray[i] = oldArray[i];
-	}
-
-	delete[] oldArray;
-	oldArray = newArray;
-
-	return newsize;
-}
