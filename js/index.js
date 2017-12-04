af7a4045f0d9f4f4d58eca55b4f496f4791fc6f2
Kevin Smith <ksmith0516@gmail.com>
2017-12-04 14:51:09 -0600

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -17,3 +17,12 @@ ipcRenderer.on('send_current_user', function(event,user){
     var username = user['username'];
     $('#user-header').text("Dashboard - " + username.substring(0,username.indexOf('@')));
 });
+
+ipcRenderer.on('load_reports', function(event,reports){
+    // display links to the previous reports and send the selected report name to the reports view as a query string
+    for (var r in reports) {
+        $('#reports-list').append($('<div/>').addClass('reports')
+            .append($('<div/>').addClass('report')
+                .append($("<a href='reports.html?reportName=" + r + "'/>").text(reports[r]['reportName']))));
+    }
+});
